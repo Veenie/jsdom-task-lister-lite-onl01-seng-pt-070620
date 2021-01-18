fcf61297ce5a8beec21c78b21791e4593e9f0824
Raveen K Sharma <raveen08@gmail.com>
2021-01-18 20:43:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  const taskForm = document.querySelector("#create-task-form");
-
-  const taskList = document.querySelector("#tasks");
-
-  taskForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const newTask = document.querySelector("#new-task-description").value;
-
-    taskList.innerHTML += `<li> ${newTask}</li>`;}
   
+
 });
