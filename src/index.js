9bbf3b9700877392772d9997e9eaf52298334008
Raveen K Sharma <raveen08@gmail.com>
2021-01-18 20:12:58 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  function displayMessage(msg) {
   document.getElementById("list").innerText = msg;
   event.preventDefault();
 });
